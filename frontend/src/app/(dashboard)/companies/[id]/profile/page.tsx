fa83f82aa958878790ec81be0b08217d01149671
licivando silva <tecnologia@lsbwebinfo.com.br>
2026-03-24 14:06:28 -0300

    fix(ui): adjust toggle absolute positioning offset

--- frontend/src/app/(dashboard)/companies/[id]/profile/page.tsx
+++ frontend/src/app/(dashboard)/companies/[id]/profile/page.tsx
@@ -702,7 +702,7 @@ export default function CompanyProfilePage() {
                         onChange={handleInputChange}
                         className="sr-only peer" 
                       />
-                      <div className="w-11 h-6 bg-slate-200 peer-focus:outline-none rounded-full peer dark:bg-slate-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-emerald-500 group-hover:shadow-md transition-all"></div>
+                      <div className="relative w-11 h-6 bg-slate-200 peer-focus:outline-none rounded-full peer dark:bg-slate-700 peer-checked:after:translate-x-[120%] peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-slate-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-slate-600 peer-checked:bg-emerald-500 group-hover:shadow-sm transition-all"></div>
                       <span className="ml-3 text-sm font-bold text-slate-700 dark:text-slate-300">Contrato Assinado?</span>
                     </label>
                   </div>
